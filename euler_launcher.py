62fed4bfa22c8a8d6141c59d7ea4986b291a31df
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-06-20 10:16:02 +0200

    update notebook

--- euler_launcher.py
+++ euler_launcher.py
@@ -3,7 +3,6 @@ import os
 cmd = 'bsub -W 48:00 -n 36 -R "rusage[mem=2000]" -R fullnode -oo log_{0}-{1}-{2}.txt python results_psd_with_augmentation.py {0} {1} {2}'
 
 def launch_simulation(sigma, order, sigma_noise):
-    sbatch_txt = txtfile.format(sigma, order, sigma_noise)
     os.system(cmd.format(sigma, order, sigma_noise))
 
 

f7dcbb1ebafca64cea4b0460bead2a764ce7b799
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-06-27 19:49:18 +0200

    update launcher

--- cscs_launcher.py
+++ cscs_launcher.py
@@ -29,11 +29,9 @@ def launch_simulation(sigma, order, sigma_noise):
 
 
 sigma = 3
-orders = [1, 2, 4]
-if sigma == 3:
-    sigma_noises = [0, 0.5, 1, 1.5, 2]
-else:
-    sigma_noises = [1, 2, 3, 4, 5]
+orders = [1]
+
+sigma_noises = [1, 2, 3, 4, 5]
 for order in orders:
     for sigma_noise in sigma_noises:
         launch_simulation(sigma, order, sigma_noise)

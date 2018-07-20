14927cefa29454749fed7b80ef20e11843b1d542
Nathanael Perraudin <nperraud@users.noreply.github.com>
2018-07-20 15:55:46 +0200

    update download script

--- download.py
+++ download.py
@@ -9,18 +9,18 @@ if __name__ == '__main__':
     url_training = 'https://zenodo.org/record/1303272/files/training.zip?download=1'
     url_readme = '...'
 
-    md5_testing = 'f910d7f52318169432ecff7ba2202346'
-    md5_training = '65d9b95712eb5eb406fb903df7e9d0c4'
+    md5_testing = '62757429ebb0a257c3d54775e08c9512'
+    md5_training = '6b0f5072481397fa8842ef99524b5482'
 
     print('Download training set')
-    utils.download(url_training, 'data')
+    # utils.download(url_training, 'data')
     assert (utils.check_md5('data/training.zip', md5_training))
     print('Extract training set')
     utils.unzip('data/training.zip', 'data')
     os.remove('data/training.zip')
 
     print('Download testing set')
-    utils.download(url_testing, 'data')
+    # utils.download(url_testing, 'data')
     assert (utils.check_md5('data/testing.zip', md5_testing))
     print('Extract testing set')
     utils.unzip('data/testing.zip', 'data')
